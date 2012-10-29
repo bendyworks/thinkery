38c21713406b86d352a18c91253bea91b8bf4feb
Bill Blondeau <bill@bendyworks.com>
2012-10-29 13:41:51 -0500

    initial creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NotSuckCms::Application.config.secret_token = 'a3c8f5a1f7c58501f77cc5fb8179f2ee12228997281c70d65f05b9973af1f24ac6da36d9384ec50e3ad39aa6a100b19713b34c91418070cd94329cedd086a1a4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+NotSuckCms::Application.config.session_store :cookie_store, key: '_not-suck-cms_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# NotSuckCms::Application.config.session_store :active_record_store
